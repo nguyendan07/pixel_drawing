7dd3232d9f818ff01e1a9e514acbc3f86edd99ff
dannv6 <dannv6@fpt.com.vn>
2019-01-17 10:08:13 +0700

    Create Pixel component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Pixel />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pixel from './components/Pixel.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Pixel
   }
 }
 </script>
---
+++ src/components/Pixel.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="pixel">
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'Pixel'
+}
+</script>
+
+
+<style scoped>
+.pixel {
+    border: 1px solid lightgray;
+    width: 30px;
+    height: 30px;
+    box-sizing: border-box;
+}
+</style>
+
