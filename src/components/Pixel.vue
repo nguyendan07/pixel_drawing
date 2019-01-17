288d63036c8712a35782032448f68f673da62482
dannv6 <dannv6@fpt.com.vn>
2019-01-17 10:19:42 +0700

    add color prop

--- src/components/Pixel.vue
+++ src/components/Pixel.vue
@@ -1,13 +1,16 @@
 <template>
-    <div class="pixel">
-        
+    <div :class="['pixel', color]">
+
     </div>
 </template>
 
 
 <script>
 export default {
-    name: 'Pixel'
+    name: 'Pixel',
+    props: {
+        color: String
+    }
 }
 </script>
 
@@ -18,6 +21,18 @@ export default {
     width: 30px;
     height: 30px;
     box-sizing: border-box;
+}
+.white {
+  background-color: white;
+}
+.lightblue {
+  background-color: rgb(0, 188, 212);
+}
+.blue {
+  background-color: rgb(3, 169, 244);
+}
+.darkblue {
+  background-color: rgb(33, 150, 243);
 }
 </style>
 
