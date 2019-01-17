12d21db5aac049d582b74f6de26b189f14b40a53
dannv6 <dannv6@fpt.com.vn>
2019-01-17 16:17:45 +0700

    update new color

--- src/components/ColorPicker.vue
+++ src/components/ColorPicker.vue
@@ -1,9 +1,22 @@
 <template>
     <div>
         <Pixel isInColorPicker color="white" :current="color == 'white' ? true : false"/>
-        <Pixel isInColorPicker color="lightblue" :current="color == 'lightblue' ? true : false"/>
+        <Pixel isInColorPicker color="silver" :current="color == 'silver' ? true : false"/>
+        <Pixel isInColorPicker color="gray" :current="color == 'gray' ? true : false" />
+        <Pixel isInColorPicker color="black" :current="color == 'black' ? true : false" />
+        <Pixel isInColorPicker color="purple" :current="color == 'purple' ? true : false"/>
+        <Pixel isInColorPicker color="fuchsia" :current="color == 'fuchsia' ? true : false" />
+        <Pixel isInColorPicker color="maroon" :current="color == 'maroon' ? true : false" />
+        <Pixel isInColorPicker color="red" :current="color == 'red' ? true : false"/>
+        <Pixel isInColorPicker color="orange" :current="color == 'orange' ? true : false" />
+        <Pixel isInColorPicker color="yellow" :current="color == 'yellow' ? true : false" />
+        <Pixel isInColorPicker color="lime" :current="color == 'lime' ? true : false"/>
+        <Pixel isInColorPicker color="green" :current="color == 'green' ? true : false" />
+        <Pixel isInColorPicker color="olive" :current="color == 'olive' ? true : false" />
+        <Pixel isInColorPicker color="teal" :current="color == 'teal' ? true : false"/>
+        <Pixel isInColorPicker color="aqua" :current="color == 'aqua' ? true : false" />
         <Pixel isInColorPicker color="blue" :current="color == 'blue' ? true : false" />
-        <Pixel isInColorPicker color="darkblue" :current="color == 'darkblue' ? true : false" />
+        <Pixel isInColorPicker color="navy" :current="color == 'navy' ? true : false" />
     </div>
 </template>
 
--- src/components/Pixel.vue
+++ src/components/Pixel.vue
@@ -34,18 +34,59 @@ export default {
     height: 30px;
     box-sizing: border-box;
 }
+
+.navy {
+  background-color: rgb(0, 31, 63);
+}
+.blue {
+  background-color: rgb(0, 116, 217);
+}
+.aqua {
+  background-color: rgb(127, 219, 255);
+}
+.teal {
+  background-color: rgb(57, 204, 204);
+}
+.olive {
+  background-color: rgb(61, 153, 112);
+}
+.green {
+  background-color: rgb(46, 204, 64);
+}
+.lime {
+  background-color: rgb(1, 255, 112);
+}
+.yellow {
+  background-color: rgb(255, 220, 0);
+}
+.orange {
+  background-color: rgb(255, 133, 27);
+}
+.red {
+  background-color: rgb(255, 65, 54);
+}
+.maroon {
+  background-color: rgb(133, 20, 75);
+}
+.fuchsia {
+  background-color: rgb(240, 18, 190);
+}
+.purple {
+  background-color: rgb(177, 13, 201);
+}
+.black {
+  background-color: rgb(17, 17, 17);
+}
+.gray {
+  background-color: rgb(170, 170, 170);
+}
+.silver {
+  background-color: rgb(221, 221, 221);
+}
 .white {
   background-color: white;
 }
-.lightblue {
-  background-color: rgb(0, 188, 212);
-}
-.blue {
-  background-color: rgb(3, 169, 244);
-}
-.darkblue {
-  background-color: rgb(33, 150, 243);
-}
+
 .pixel.current {
   border: 4px solid yellow;
 }
